20ae84236fae8d83011a07ce0cfe80129bc8582d
ErnestWorldProgrammer <169020960+ErnestWorldProgrammer@users.noreply.github.com>
2024-11-14 18:11:34 +0600

    Add files via upload

---
+++ тест/app.js
@@ -0,0 +1,21 @@
+
+// ex1
+// let alert = prompt()
+// let regExp = /[0-9]/
+// let examination = () => {
+//     if (alert.match(regExp)){
+//         console.log(true)
+//     }else {
+//         console.log(false)
+//     }
+// }
+// examination()
+//
+
+
+
+// ex2
+// let masage = () => {
+//     console.log("hello")
+// }
+// setInterval(masage, 1000)
